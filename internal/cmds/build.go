dddd436ad1faf5e11da1c64db1e11ea217560747
Edgaru089 <sjs333@outlook.com>
2022-02-08 14:16:55 +0800

    build & run commands

--- internal/cmds/build.go
+++ internal/cmds/build.go
@@ -1,7 +1,11 @@
 package cmds
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -15,15 +19,77 @@ var (
 given (just like "cppgo generate"), and invokes "go build" at the given
 directory. Arguments after a double dash "--" are passed to "go build".`,
 		DisableFlagsInUseLine: true,
-		Run:                   RunBuild,
+		RunE:                  RunBuildRun,
+	}
+
+	cmdRun = &cobra.Command{
+		Use:   "run [options] [path] [ -- [go-build options]]",
+		Short: "Generate & run a package with cppgo source files",
+		Long: `run updates all the changed cppgo source files in the go module path
+given (just like "cppgo generate"), and invokes "go run ." at the given
+directory. Arguments after a double dash "--" are passed to "go run".`,
+		DisableFlagsInUseLine: true,
+		RunE:                  RunBuildRun,
 	}
 )
 
-// RunBuild executes the build command.
-func RunBuild(cmd *cobra.Command, args []string) {
-	fmt.Printf("Hi!\n  Verbose: %v, Extensions: %v\n  Args:", Flags.Verbose, Flags.Extensions)
-	for _, s := range args {
-		fmt.Printf(` "%s"`, s)
+func init() {
+	cmdBuild.Flags().StringSliceVarP(&rawDefines, "define", "D", nil, "pre-defined macros (as NAME=VAL,NAME=VAL)")
+	cmdRun.Flags().StringSliceVarP(&rawDefines, "define", "D", nil, "pre-defined macros (as NAME=VAL,NAME=VAL)")
+}
+
+// RunBuildRun executes the build and run command.
+func RunBuildRun(cmd *cobra.Command, args []string) error {
+
+	// Split the target directory and "go build" flags
+	var dirs, flags []string
+	if strings.HasPrefix(cmd.Use, "build") {
+		flags = []string{"build"}
+	} else if strings.HasPrefix(cmd.Use, "run") {
+		flags = []string{"run", "."}
 	}
-	fmt.Println()
+
+	isdirs := true
+	for _, str := range args {
+		if len(str) > 0 {
+			if str[0] == '-' {
+				isdirs = false
+			}
+
+			if isdirs {
+				dirs = append(dirs, str)
+			} else {
+				flags = append(flags, str)
+			}
+		}
+	}
+
+	// Generate
+	err := RunGenerate(nil, dirs)
+	if err != nil {
+		return err
+	}
+
+	// Build
+	var builddir string
+	if len(dirs) > 0 {
+		builddir = dirs[0]
+	} else {
+		builddir, err = os.Getwd()
+		if err != nil {
+			return errors.New("build: working directory cannot be retrieved and none specified")
+		}
+	}
+
+	if Flags.Verbose {
+		fmt.Printf("Build/Run, At: %s, Invoke: go %v\n", builddir, flags)
+	}
+
+	gocmd := exec.Command(Flags.GoBinary, flags...)
+	gocmd.Dir = builddir
+	gocmd.Stdout = os.Stdout
+	gocmd.Stderr = os.Stderr
+	cobra.CheckErr(gocmd.Run())
+
+	return nil
 }
--- internal/cmds/rootcmd.go
+++ internal/cmds/rootcmd.go
@@ -16,6 +16,7 @@ with the extension "cppgo", among other things. Run "cppgo doc" for more info.`,
 	commands = []*cobra.Command{
 		cmdGenerate,
 		cmdBuild,
+		cmdRun,
 		cmdDoc,
 	}
 )
